7d7ece66aa4d4c39739536e31eb942dc38569732
3top1a <57371001+3top1a@users.noreply.github.com>
2020-04-25 17:16:47 +0200

    So i AM retarded.

--- Source/src/main/java/_3top1a/macc_client/Connection.java
+++ Source/src/main/java/_3top1a/macc_client/Connection.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.DecimalFormat;
@@ -12,81 +11,106 @@ import java.text.NumberFormat;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class Connection {
 	static ServerSocket ss = null;
 	static Socket socket = null;
 	static Thread th = null;
 	static NumberFormat formatter = null;
-	
-	public static void run() throws Exception {
-		ss = new ServerSocket(6667);
-		
-		while (true) {
-			socket = ss.accept();
 
-			do {
-				ReceiveData();
-				SendData();
-			} 
-			while(!ss.isClosed());
+	public static class MyClass implements Runnable {
+		@Override
+		public void run() {
+			try {
+				ss = new ServerSocket(6667);
+			} catch (IOException e1) {}
+
+			while (true) {
+
+				try {
+					socket = ss.accept();
+				} catch (IOException e1) {}
+
+				ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+				exec.scheduleAtFixedRate(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							SendData();
+							ReceiveData();
+						} catch (Exception e) {}
+					}
+				}, 0, (1000 / 10), TimeUnit.MILLISECONDS);
+			}
 		}
 	}
 
 	public static void Connect() throws Exception {
 		formatter = new DecimalFormat("#0.0");
-		
-		run();
+
+		th = new Thread(new MyClass());
+		th.start();
 	}
 
 	public static void SendData() throws Exception, IOException {
 		ObjectOutputStream dOut = new ObjectOutputStream(socket.getOutputStream());
-
-		String x = formatter.format(Minecraft.getMinecraft().player.posX);
-		String y = formatter.format(Minecraft.getMinecraft().player.posY);
-		String z = formatter.format(Minecraft.getMinecraft().player.posZ);
-
-		String hp = formatter.format(Minecraft.getMinecraft().player.getHealth());
-		String maxhp = formatter.format(Minecraft.getMinecraft().player.getMaxHealth());
-
-		String name = Minecraft.getMinecraft().player.getName();
-
-		int dimension = Minecraft.getMinecraft().player.dimension;
-
-		int expLevel = Minecraft.getMinecraft().player.experienceLevel;
-
-		dOut.writeUTF(" " + x + " " + y + " " + z + " " + hp + " " + maxhp + " " + name + " " + dimension + " "
-				+ expLevel + " ");
+		
+		if(Minecraft.getMinecraft().isGamePaused() && Minecraft.getMinecraft().player == null)
+		{
+			//We are in the main menu
+			
+			dOut.writeUTF(" 101 ");
+		}		
+		else 
+		{
+			String x = formatter.format(Minecraft.getMinecraft().player.posX);
+			String y = formatter.format(Minecraft.getMinecraft().player.posY);
+			String z = formatter.format(Minecraft.getMinecraft().player.posZ);
+	
+			String hp = formatter.format(Minecraft.getMinecraft().player.getHealth());
+			String maxhp = formatter.format(Minecraft.getMinecraft().player.getMaxHealth());
+	
+			String name = Minecraft.getMinecraft().player.getName();
+	
+			int dimension = Minecraft.getMinecraft().player.dimension;
+	
+			int expLevel = Minecraft.getMinecraft().player.experienceLevel;
+	
+			dOut.writeUTF(" " + x + " " + y + " " + z + " " + hp + " " + maxhp + " " + name + " " + dimension + " "
+					+ expLevel + " ");
+		}
 		dOut.flush();
 	}
 
 	public static void ReceiveData() throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader((socket.getInputStream())));
-		String Data;
-		if ((Data = in.readLine().toString()) != null) 
+		String data;
+		if ((data = in.readLine()) != null) 
 		{
-			System.out.println("\r\nMessage" + Data);
+			System.out.println("\r\nMessage " + data);
 			
-			PrintWriter writer = new PrintWriter("DEBUG.txt", "UTF-8");
-			writer.println(Data);
-			writer.close();
-			
-			
-			if(regex("goto.*", Data))
+			if(data.startsWith("exit"))
 			{
+				//TODO
+				//Minecraft.getMinecraft().player.
+			}
+			 
+			if(data.startsWith("goto"))
+			{
+				//TODO
 				System.out.println("GOING TO FUCK MYSELF");
-				
+
 				Minecraft.getMinecraft().player.sendChatMessage(".b goto 0 0 0");
+			}
+			
+			if(data.startsWith("IHAVENOIDEA"))
+			{
+				//NOT TODO
 			}
 		}
 
-	}
-	
-	public static boolean regex(String re, String to)
-	{
-		return Pattern.compile(re).matcher(to).matches();
 	}
 }
